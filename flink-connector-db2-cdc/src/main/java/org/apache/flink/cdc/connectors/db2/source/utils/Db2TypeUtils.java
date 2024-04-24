apache/flink-cdc

[cdc-connector][db2] Db2 support incremental source

flink-cdc-connect/flink-cdc-source-connectors/flink-connector-db2-cdc/src/main/java/org/apache/flink/cdc/connectors/db2/source/utils/Db2TypeUtils.java
@@ -1,0 +1,80 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.cdc.connectors.db2.source.utils;
+
+import org.apache.flink.table.api.DataTypes;
+import org.apache.flink.table.types.DataType;
+
+import io.debezium.relational.Column;
+
+import java.sql.Types;
+
+/** Utilities for converting from Db2 types to Flink types. */
+public class Db2TypeUtils {
+
+    /** Returns a corresponding Flink data type from a debezium {@link Column}. */
+    public static DataType fromDbzColumn(Column column) {
+        DataType dataType = convertFromColumn(column);
+        if (column.isOptional()) {
+            return dataType;
+        } else {
+            return dataType.notNull();
+        }
+    }
+
+    /**
+     * Returns a corresponding Flink data type from a debezium {@link Column} with nullable always
+     * be true.
+     */
+    private static DataType convertFromColumn(Column column) {
+        switch (column.jdbcType()) {
+            case Types.CHAR:
+            case Types.VARCHAR:
+            case Types.SQLXML:
+                return DataTypes.STRING();
+            case Types.CLOB:
+            case Types.BLOB:
+            case Types.BINARY:
+            case Types.VARBINARY:
+                return DataTypes.BYTES();
+            case Types.INTEGER:
+            case Types.SMALLINT:
+                return DataTypes.INT();
+            case Types.BIGINT:
+                return DataTypes.BIGINT();
+            case Types.FLOAT:
+            case Types.REAL:
+            case Types.DOUBLE:
+            case Types.DECIMAL:
+            case Types.NUMERIC:
+                return DataTypes.DECIMAL(column.length(), column.scale().orElse(0));
+            case Types.DATE:
+                return DataTypes.DATE();
+            case Types.TIMESTAMP:
+            case Types.TIMESTAMP_WITH_TIMEZONE:
+                return column.length() >= 0
+                        ? DataTypes.TIMESTAMP(column.length())
+                        : DataTypes.TIMESTAMP();
+            default:
+                throw new UnsupportedOperationException(
+                        String.format(
+                                "Don't support SqlSever type '%s' yet, jdbcType:'%s'.",
+                                column.typeName(), column.jdbcType()));
+        }
+    }
+}

eskabetxe/flink-connector-debezium

flink-connector-db2-cdc/src/main/java/org/apache/flink/cdc/connectors/db2/source/utils/Db2TypeUtils.java
@@ -1,0 +1,83 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.apache.flink.cdc.connectors.db2.source.utils;
+
+import org.apache.flink.table.api.DataTypes;
+import org.apache.flink.table.types.DataType;
+
+import io.debezium.relational.Column;
+
+import java.sql.Types;
+
+/** Utilities for converting from Db2 types to Flink types. */
+public class Db2TypeUtils {
+
+    /** Returns a corresponding Flink data type from a debezium {@link Column}. */
+    public static DataType fromDbzColumn(Column column) {
+        DataType dataType = convertFromColumn(column);
+        if (column.isOptional()) {
+            return dataType;
+        } else {
+            return dataType.notNull();
+        }
+    }
+
+    /**
+     * Returns a corresponding Flink data type from a debezium {@link Column} with nullable always
+     * be true.copy from io.debezium.connector.db2.Db2ValueConverters#converte.
+     */
+    private static DataType convertFromColumn(Column column) {
+        switch (column.jdbcType()) {
+            case Types.CHAR:
+            case Types.VARCHAR:
+            case Types.SQLXML:
+            case Types.CLOB:
+                return DataTypes.STRING();
+            case Types.BLOB:
+            case Types.BINARY:
+            case Types.VARBINARY:
+                return DataTypes.BYTES();
+            case Types.TINYINT:
+            case Types.SMALLINT:
+                return DataTypes.SMALLINT();
+            case Types.INTEGER:
+                return DataTypes.INT();
+            case Types.BIGINT:
+                return DataTypes.BIGINT();
+            case Types.FLOAT:
+            case Types.REAL:
+                return DataTypes.FLOAT();
+            case Types.DOUBLE:
+                return DataTypes.DOUBLE();
+            case Types.DECIMAL:
+            case Types.NUMERIC:
+                return DataTypes.DECIMAL(column.length(), column.scale().orElse(0));
+            case Types.DATE:
+                return DataTypes.DATE();
+            case Types.TIMESTAMP:
+                return column.length() >= 0
+                        ? DataTypes.TIMESTAMP(column.length())
+                        : DataTypes.TIMESTAMP();
+            default:
+                throw new UnsupportedOperationException(
+                        String.format(
+                                "Don't support DB2 type '%s' yet, jdbcType:'%s'.",
+                                column.typeName(), column.jdbcType()));
+        }
+    }
+}